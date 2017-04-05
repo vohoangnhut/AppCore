2c8fc194b2ea2c4f1e18a65640795f90d81ffe46
vohoangnhut <nhut.vo@dou.com.vn>
2017-04-05 17:37:18 +0700

    audit

--- models/index.js
+++ models/index.js
@@ -5,7 +5,12 @@ const dotenv = require('dotenv')
 //Load env file
 dotenv.load({path : path.join(__dirname,'../.env')})
 
-const db_sequelize = new Sequelize(process.env.DATABASE_URL);
+
+//CLEARDB_DATABASE_URL on heroku
+if(process.env.CLEARDB_DATABASE_URL)
+    const db_sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
+else
+    const db_sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const User = db_sequelize.import(path.join(__dirname,'user.js'))
 
