c6340a540dbdd8bf34c9c79b362556fe0b7beff3
vohoangnhut <nhut.vo@dou.com.vn>
2017-04-14 11:02:15 +0700

    deploy 2

--- config/auth.js
+++ config/auth.js
@@ -1,4 +1,5 @@
 // config/auth.js
+const path = require('path')
 const dotenv = require('dotenv')
 dotenv.load({path : path.join(__dirname,'../.env')})
 
@@ -8,7 +9,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '435367986812872', // your App ID
         'clientSecret'  : '9f8f6d1e4c96dd5624a5634980e77582', // your App Secret
-        'callbackURL'   : process.env.FACEBOOK_CALL_BACK//'https://feckingawesome.herokuapp.com/auth/facebook/callback'
+        'callbackURL'   : process.env.FACEBOOK_CALL_BACK
         
         //'callbackURL'   : 'http://192.168.1.9:8080/auth/facebook/callback'//'http://localhost:8080/auth/facebook/callback'
     },
