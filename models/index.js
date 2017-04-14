087bfacf15a30a18713d4da55e92efd5fffa5075
vohoangnhut <nhut.vo@dou.com.vn>
2017-04-14 15:39:38 +0700

    deploy 2

--- models/index.js
+++ models/index.js
@@ -4,21 +4,22 @@ const dotenv = require('dotenv')
 
 //Load env file
 dotenv.load({path : path.join(__dirname,'../.env')})
-//const db_sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
+const db_sequelize = new Sequelize(process.env.JAWSDB_URL);
+console.log(process.env.JAWSDB_URL);
 //CLEARDB_DATABASE_URL=mysql://bc49495ded2948:d61c3548@us-cdbr-iron-east-03.cleardb.net/heroku_635a910a6589203?reconnect=true
 //DEV_DATABASE_URL=mysql://root:Teo54321@localhost:3306/db_node
-const db_sequelize = new Sequelize('heroku_635a910a6589203', 'bc49495ded2948', 'd61c3548', {
+// const db_sequelize = new Sequelize('heroku_635a910a6589203', 'bc49495ded2948', 'd61c3548', {
 
-    host: 'us-cdbr-iron-east-03.cleardb.net',
-    port: 3306,
-    logging: false,
-    dialect: 'mysql',
-    pool: {
-        maxConnections: 5,
-        maxIdleTime: 30
-    }
+//     host: 'us-cdbr-iron-east-03.cleardb.net',
+//     port: 3306,
+//     logging: false,
+//     dialect: 'mysql',
+//     pool: {
+//         maxConnections: 5,
+//         maxIdleTime: 30
+//     }
 
-});
+// });
 
 
 const User = db_sequelize.import(path.join(__dirname,'user.js'))
